f8b3e864113abdb4990f1316b383f7b2343bb576
Stefano Novelli <murdercode@gmail.com>
2023-10-11 16:13:49 +0200

    Update admin-bar configuration for max-width, adminUrl, uri, wildcard, targetEndpointUrl, and parameterToReturn

--- config/admin-bar.php
+++ config/admin-bar.php
@@ -5,28 +5,28 @@ return [
     'style' => [
         'show3labsLogo' => true,
         'theme' => 'dark', // 'light' or 'dark'
-        'max-width' => '1000px',
+        'max-width' => '1200px',
         'position' => 'bottom', // 'top' or 'bottom'
         'font-size' => '14px',
     ],
     'config' => [
-        'adminUrl' => '/nova',
+        'adminUrl' => '/cms',
         'editPost' => [
             'enabled' => true,
-            'uri' => 'posts/{post}', // detect the current route as defined in your routes/web.php
+            'uris' => ['articoli/{slug}', 'notizie/{slug}', 'recensioni/{slug}'], // detect the current route as defined in your routes/web.php
+            'wildcard' => 'slug', // the wildcard for the parameter (as over)
+            'parameterForSearch' => 'slug', // the parameter to be used for search the post
+            'model' => \App\Models\Post::class, // the model to be used for search the post
+            'parameterToReturn' => 'id', // the value to be returned
+            'targetEndpointUrl' => '/cms/resources/posts/{parameter}',
+        ],
+        'emptyCachePost' => [
+            'enabled' => true,
+            'uris' => ['articoli/{slug}', 'notizie/{slug}', 'recensioni/{slug}'], // detect the current route as defined in your routes/web.php
             'wildcard' => 'post', // the wildcard for the parameter (as over)
             'parameterForSearch' => 'slug', // the parameter to be used for search the post
             'model' => \App\Models\Post::class, // the model to be used for search the post
             'parameterToReturn' => 'id', // the value to be returned
-            'targetEndpointUrl' => '/nova/resources/posts/{parameter}',
-        ],
-        'emptyCachePost' => [
-            'enabled' => true,
-            'uri' => 'posts/{post}', // detect the current route as defined in your routes/web.php
-            'wildcard' => 'post', // the wildcard for the parameter (as over)
-            'parameterForSearch' => 'slug', // the parameter to be used for search the post
-            'model' => \App\Models\Post::class, // the model to be used for search the post
-            'parameterToReturn' => 'slug', // the value to be returned
             'targetEndpointUrl' => '/posts/{parameter}?nocache',
         ],
     ],
