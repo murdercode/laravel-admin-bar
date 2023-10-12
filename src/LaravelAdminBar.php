b39f5698caa574e0a8a4675c652a9bb319100344
Stefano Novelli <murdercode@gmail.com>
2023-10-12 10:14:10 +0200

    Refactor LaravelAdminBar: remove getPostEmptyCacheLink and optimize generateLink

--- src/LaravelAdminBar.php
+++ src/LaravelAdminBar.php
@@ -14,7 +14,6 @@ class LaravelAdminBar
     {
         $postEditLink = self::getPostEditLink();
         //        $postEmptyCacheLink = self::getPostEmptyCacheLink();
-
         return view('admin-bar::render', compact('postEditLink'));
     }
 
@@ -26,51 +25,49 @@ class LaravelAdminBar
         $wildcard = config('admin-bar.config.editPost.wildcard');
         $parameterToReturn = config('admin-bar.config.editPost.parameterToReturn');
         $enabled = config('admin-bar.config.editPost.enabled');
-        $currentRoute = Route::current();
         $targetEndpointUrl = config('admin-bar.config.editPost.targetEndpointUrl');
 
-        return self::generateLink($uris, $currentRoute, $enabled, $model, $parameterForSearch, $wildcard, $parameterToReturn, $targetEndpointUrl);
+        return self::generateLink($uris, $enabled, $model, $parameterForSearch, $wildcard, $parameterToReturn, $targetEndpointUrl);
     }
 
-    public static function getPostEmptyCacheLink(): ?string
-    {
-        $uris = config('admin-bar.config.emptyCachePost.uris');
-        $parameterForSearch = config('admin-bar.config.emptyCachePost.parameterForSearch');
-        $model = config('admin-bar.config.emptyCachePost.model');
-        $wildcard = config('admin-bar.config.emptyCachePost.wildcard');
-        $parameterToReturn = config('admin-bar.config.emptyCachePost.parameterToReturn');
-        $enabled = config('admin-bar.config.emptyCachePost.enabled');
-        $currentRoute = Route::current();
-        $targetEndpointUrl = config('admin-bar.config.emptyCachePost.targetEndpointUrl');
-
-        return self::generateLink(
-            $uris,
-            $currentRoute,
-            $enabled,
-            $model,
-            $parameterForSearch,
-            $wildcard,
-            $parameterToReturn,
-            $targetEndpointUrl
-        );
-    }
+//    public static function getPostEmptyCacheLink(): ?string
+//    {
+//        $uris = config('admin-bar.config.emptyCachePost.uris');
+//        $parameterForSearch = config('admin-bar.config.emptyCachePost.parameterForSearch');
+//        $model = config('admin-bar.config.emptyCachePost.model');
+//        $wildcard = config('admin-bar.config.emptyCachePost.wildcard');
+//        $parameterToReturn = config('admin-bar.config.emptyCachePost.parameterToReturn');
+//        $enabled = config('admin-bar.config.emptyCachePost.enabled');
+//        $currentRoute = Route::current();
+//        $targetEndpointUrl = config('admin-bar.config.emptyCachePost.targetEndpointUrl');
+//
+//        return self::generateLink(
+//            $uris,
+//            $currentRoute,
+//            $enabled,
+//            $model,
+//            $parameterForSearch,
+//            $wildcard,
+//            $parameterToReturn,
+//            $targetEndpointUrl
+//        );
+//    }
 
     /**
      * @return array|Repository|Application|mixed|string|string[]|null
      */
-    public static function generateLink(mixed $uris, ?\Illuminate\Routing\Route $currentRoute, mixed $enabled, mixed $model, mixed $parameterForSearch, mixed $wildcard, mixed $parameterToReturn, mixed $targetEndpointUrl): mixed
+    public static function generateLink(mixed $uris, mixed $enabled, mixed $model, mixed $parameterForSearch, mixed $wildcard, mixed $parameterToReturn, mixed $targetEndpointUrl): mixed
     {
+        $currentRoute = Route::current() ?: null;
         foreach ($uris as $uri) {
-            if ($uri && $currentRoute->uri == $uri) {
+            if ($uri && $currentRoute && $currentRoute->uri == $uri) {
                 $post = $model::where($parameterForSearch, $currentRoute->parameters[$wildcard])->first();
                 if ($post) {
                     return str_replace('{parameter}', $post->{$parameterToReturn}, $targetEndpointUrl);
                 }
-
                 return null;
             }
         }
-
         return null;
     }
 }
